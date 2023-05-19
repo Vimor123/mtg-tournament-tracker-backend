efb16fa2854825e155756b5955516e52afe3c3f2
Vimor123 <vmladar1@gmail.com>
2023-05-19 08:45:25 +0200

    Initialized Spring Boot Project

---
+++ src/main/java/mttb/MtgTournamentTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package mttb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgTournamentTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgTournamentTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mttb/MtgTournamentTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mttb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgTournamentTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
